b7346396a734a2ae69ad867c1b6ff1624a6a9a4a
Midhulaj <mail2midhulaj@gmail.com>
2025-06-15 20:17:06 +0400

    Initial Commit

---
+++ src/ble/ble_service.c
@@ -0,0 +1,117 @@
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/kernel.h>
+#include <zephyr/init.h>
+#include <zephyr/sys/check.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include "ble_service.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(bt_sevice,LOG_LEVEL_DBG);
+
+
+static uint8_t service_blsc;
+static sys_slist_t service_cbs = SYS_SLIST_STATIC_INIT(&service_cbs);
+
+static void service_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value){
+	ARG_UNUSED(attr);
+
+	struct bt_service_cb *listener;
+
+	bool notif_enabled = (value == BT_GATT_CCC_NOTIFY);
+
+	LOG_INF("Service notifications %s", notif_enabled ? "enabled" : "disabled");
+
+	SYS_SLIST_FOR_EACH_CONTAINER(&service_cbs, listener, _node) {
+		if (listener->ntf_changed) {
+			listener->ntf_changed(notif_enabled);
+		}
+	}
+}
+
+static ssize_t read_blsc(struct bt_conn *conn, const struct bt_gatt_attr *attr,void *buf, uint16_t len, uint16_t offset){
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, &service_blsc,sizeof(service_blsc));
+}
+
+static ssize_t ctrl_point_write(struct bt_conn *conn, const struct bt_gatt_attr *attr,const void *buf, uint16_t len, uint16_t offset, uint8_t flags){
+	int err = -ENOTSUP;
+	struct bt_service_cb *listener;
+
+	LOG_INF("CTRL Point Written %d", len);
+
+	SYS_SLIST_FOR_EACH_CONTAINER(&service_cbs, listener, _node) {
+		if (listener->ctrl_point_write) {
+			err = listener->ctrl_point_write(*((uint8_t *)buf));
+			/* If we get an error other than ENOTSUP then immediately
+			 * break the loop and return a generic gatt error, assuming this
+			 * listener supports this request code, but failed to serve it
+			 */
+			if ((err != 0) && (err != -ENOTSUP)) {
+				return BT_GATT_ERR(BT_ATT_ERR_UNLIKELY);
+			}
+		}
+	}
+	if (err) {
+		return BT_GATT_ERR(-1);
+	} else {
+		return len;
+	}
+}
+
+/* Heart Rate Service Declaration */
+BT_GATT_SERVICE_DEFINE(my_svc,
+	BT_GATT_PRIMARY_SERVICE(SERVICE_UUID),
+	BT_GATT_CHARACTERISTIC(CHARACTERISTIC_UUID, 
+                BT_GATT_CHRC_WRITE | BT_GATT_CHRC_READ|BT_GATT_CHRC_NOTIFY,
+			    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE,
+                read_blsc,ctrl_point_write, NULL),
+	BT_GATT_CCC(service_ccc_cfg_changed,
+		        BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+);
+
+static int bt_service_init(void){
+	service_blsc = 0x01;
+	return 0;
+}
+
+int bt_service_cb_register(struct bt_service_cb *cb){
+    CHECKIF(cb == NULL) {
+		return -EINVAL;
+	}
+	sys_slist_append(&service_cbs, &cb->_node);
+	return 0;
+}
+
+int bt_service_cb_unregister(struct bt_service_cb *cb){
+    CHECKIF(cb == NULL) {
+		return -EINVAL;
+	}
+	if (!sys_slist_find_and_remove(&service_cbs, &cb->_node)) {
+		return -ENOENT;
+	}
+	return 0;
+}
+
+int bt_service_notify(uint16_t heartrate){
+    int rc;
+	static uint8_t hrm[2];
+
+	hrm[0] = 0x06; /* uint8, sensor contact */
+	hrm[1] = heartrate;
+
+	rc = bt_gatt_notify(NULL, &my_svc.attrs[1], &hrm, sizeof(hrm));
+
+	return rc == -ENOTCONN ? 0 : rc;
+}
+
+
+SYS_INIT(bt_service_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+
---
+++ src/ble/ble_service.h
@@ -0,0 +1,90 @@
+#ifndef BLE_SERVICE_H_
+#define BLE_SERVICE_H_
+
+#include <stdint.h>
+
+#include <zephyr/sys/slist.h>
+
+
+#define SERVICE_UUID   BT_UUID_DECLARE_128(BT_UUID_128_ENCODE(0x17DFAF86,0x8765,0x4A44,0xB6B8,0xC838F7FA5620))
+/**
+ *  @brief Characteristic UUID Value
+ */
+#define CHARACTERISTIC_UUID_VAL 0xFF01
+/**
+ *  @brief Characteristic Control Point
+ */
+#define CHARACTERISTIC_UUID \
+	BT_UUID_DECLARE_16(CHARACTERISTIC_UUID_VAL)
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief service callback structure */
+struct bt_service_cb {
+	/** @brief Sevice notifications changed
+	 *
+	 * @param enabled Flag that is true if notifications were enabled, false
+	 *                if they were disabled.
+	 */
+	void (*ntf_changed)(bool enabled);
+
+	/**
+	 * @brief sevice write callback
+	 *
+	 * @param request control point request code
+	 *
+	 * @return 0 on successful handling of control point request
+	 * @return -ENOTSUP if not supported. It can be used to pass handling to other
+	 *         listeners in case of multiple listeners
+	 * @return other negative error codes will result in immediate error response
+	 */
+	int (*ctrl_point_write)(uint8_t request);
+
+	/** Internal member to form a list of callbacks */
+	sys_snode_t _node;
+};
+
+/** @brief Heart rate service callback register
+ *
+ * This function will register callbacks that will be called in
+ * certain events related to Heart rate service.
+ *
+ * @param cb Pointer to callbacks structure. Must point to memory that remains valid
+ * until unregistered.
+ *
+ * @return 0 on success
+ * @return -EINVAL in case @p cb is NULL
+ */
+int bt_service_cb_register(struct bt_service_cb *cb);
+
+/** @brief Heart rate service callback unregister
+ *
+ * This function will unregister callback from Heart rate service.
+ *
+ * @param cb Pointer to callbacks structure
+ *
+ * @return 0 on success
+ * @return -EINVAL in case @p cb is NULL
+ * @return -ENOENT in case the @p cb was not found in registered callbacks
+ */
+int bt_service_cb_unregister(struct bt_service_cb *cb);
+
+/** @brief Notify heart rate measurement.
+ *
+ * This will send a GATT notification to all current subscribers.
+ *
+ *  @param heartrate The heartrate measurement in beats per minute.
+ *
+ *  @return Zero in case of success and error code in case of error.
+ */
+int bt_service_notify(uint16_t heartrate);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !BLE_SERVICE_H_ */
